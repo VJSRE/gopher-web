a195c99968987f22cbee68fca3e1e7f01d735a5d
VJSRE <mykoneru@gmail.com>
2025-09-18 10:30:48 -0700

    add pathHanlder

--- main.go
+++ main.go
@@ -5,14 +5,30 @@ import (
 	"net/http"
 )
 
-func handleFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "<h1>Welcome to my awesome World !!</h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:VJSRE@example.com\">VJSRE@example.com </a>.")
 
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+
+}
 func main() {
-	http.HandleFunc("/", handleFunc)
+	http.HandleFunc("/", pathHandler)
 	fmt.Println("Listening on port 8000")
 	http.ListenAndServe(":8000", nil)
 }
