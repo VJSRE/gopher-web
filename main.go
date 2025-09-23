66a24969e710d6b071fe7fb364932435972a21ac
VJSRE <mykoneru@gmail.com>
2025-09-23 09:58:02 -0700

    Add templates to faq page

--- main.go
+++ main.go
@@ -8,10 +8,9 @@ import (
 	"path/filepath"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+func executeTemplate(w http.ResponseWriter, filePath string) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tplPath := filepath.Join("templates", "home.gohtml")
-	tpl, err := template.ParseFiles(tplPath)
+	tpl, err := template.ParseFiles(filePath)
 	if err != nil {
 		fmt.Printf("parsing template: %v", err)
 		http.Error(w, "There is an error while parsing the template", http.StatusInternalServerError)
@@ -25,28 +24,25 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tplPath := filepath.Join("templates", "contact.gohtml")
-	tpl, err := template.ParseFiles(tplPath)
-	if err != nil {
-		fmt.Printf("parsing template: %v", err)
-		http.Error(w, "There is an error while parsing the template", http.StatusInternalServerError)
-		return
-	}
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		fmt.Printf("executing template: %v", err)
-		http.Error(w, "There is an error while executing the template", http.StatusInternalServerError)
-		return
-	}
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "home.gohtml")
+	executeTemplate(w, tplPath)
+}
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	tplPath := filepath.Join("templates", "contact.gohtml")
+	executeTemplate(w, tplPath)
+}
+
+func fqlHandler(w http.ResponseWriter, r *http.Request) {
+	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
 }
 
 func main() {
 	r := chi.NewRouter()
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
+	r.Get("/faq", fqlHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "404 page not found", http.StatusNotFound)
 	})
