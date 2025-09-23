6327458b49f8f8809e70974d443cb71da4360f53
VJSRE <mykoneru@gmail.com>
2025-09-22 17:52:31 -0700

    Add templates to homehandler

--- main.go
+++ main.go
@@ -3,12 +3,26 @@ package main
 import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
+	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Welcome to my awesome World !!</h1>")
+	tplPath := filepath.Join("templates", "home.gohtml")
+	tpl, err := template.ParseFiles(tplPath)
+	if err != nil {
+		fmt.Printf("parsing template: %v", err)
+		http.Error(w, "There is an error while parsing the template", http.StatusInternalServerError)
+		return
+	}
+	err = tpl.Execute(w, nil)
+	if err != nil {
+		fmt.Printf("executing template: %v", err)
+		http.Error(w, "There is an error while executing the template", http.StatusInternalServerError)
+		return
+	}
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
