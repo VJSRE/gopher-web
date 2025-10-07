dea1e91180d6610376b934e732f6c8c62d15089f
VJSRE <mykoneru@gmail.com>
2025-10-07 09:42:24 -0700

    Add bcrypt package

---
+++ cmd/bcrypt/bcrypt.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"os"
+)
+
+func main() {
+
+	switch os.Args[1] {
+	case "hash":
+		hash(os.Args[2])
+	case "compare":
+		compare(os.Args[2], os.Args[3])
+	}
+}
+
+func hash(password string) {
+	hashBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Printf("error hashing: %v\n", err)
+		return
+	}
+	fmt.Println(string(hashBytes))
+}
+
+func compare(password string, hash string) {
+	fmt.Println("TODO: compare password: %q wtih the hash: %q ", password, hash)
+}
