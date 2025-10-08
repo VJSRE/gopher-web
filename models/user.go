cf41624fbc43503fe74c67714bebbd0775b02a6b
VJSRE <mykoneru@gmail.com>
2025-10-08 10:21:18 -0700

    Add postgres connection to the model

--- models/user.go
+++ models/user.go
@@ -1,7 +1,45 @@
 package models
 
+import (
+	"database/sql"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"strings"
+)
+
 type User struct {
 	ID           int
 	Email        string
 	PasswordHash string
+}
+
+type UserService struct {
+	DB *sql.DB
+}
+
+type NewUser struct {
+	Email    string
+	Password string
+}
+
+func (us *UserService) Create(email, password string) (*User, error) {
+	email = strings.ToLower(email)
+	hashBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	if err != nil {
+		return nil, fmt.Errorf("error while hashing password: %v", err)
+	}
+	passwordHash := string(hashBytes)
+
+	user := User{
+		Email:        email,
+		PasswordHash: passwordHash,
+	}
+
+	row := us.DB.QueryRow(`INSERT INTO users (email,password_hash)
+	VALUES ($1,$2) RETURNING id; `, email, passwordHash)
+	err = row.Scan(&user.ID)
+	if err != nil {
+		return nil, fmt.Errorf("error while creating user: %v", err)
+	}
+	return &user, nil
 }
