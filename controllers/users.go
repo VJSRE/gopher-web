03d9e38a41e6cbf0895511ffe14b229db09a148e
VJSRE <mykoneru@gmail.com>
2025-10-08 11:33:22 -0700

    register an user

--- controllers/users.go
+++ controllers/users.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"github.com/VJSRE/lenslocked/models"
 	"net/http"
 )
 
@@ -9,6 +10,7 @@ type Users struct {
 	Template struct {
 		New Template
 	}
+	UserService *models.UserService
 }
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
@@ -16,6 +18,13 @@ func (u Users) New(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Email: ", r.FormValue("email"))
-	fmt.Fprint(w, "Password: ", r.FormValue("password"))
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	user, err := u.UserService.Create(email, password)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "something went wrong", http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintf(w, "User created successfully:\n%s", user)
 }
